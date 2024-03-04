ae864a33408ccddf8f3a7d031f8d78f5dc3aebf4
TakumaNaito227 <63332756+TakumaNaito227@users.noreply.github.com>
2024-03-04 23:12:37 +0900

    fix index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,46 @@
 <template>
     <div>
-        <!-- ボタンのクリックがあったらgetMsgメソッド実行 -->
-        <button type="button" name="button" @click="getMsg">
-            RailsからAPIを取得する
-        </button>
-        <!-- 取得したメッセージを表示(ループ) -->
-        <div v-for="(msg, i) in msgs" :key="i">
-            {{ msg }}
-        </div>
+        <h2>Usersテーブルの取得</h2>
+        <table v-if="users.length">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>email</th>
+                    <th>created_at</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(user, i) in users" :key="`user-${i}`">
+                    <td>{{ user.id }}</td>
+                    <td>{{ user.name }}</td>
+                    <td>{{ user.email }}</td>
+                    <td>{{ dateFormat(user.created_at) }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <div v-else>ユーザーが取得できませんでした</div>
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            msgs: [],
-        };
+    async asyncData({ $axios }) {
+        let users = [];
+        await $axios.$get("/api/v1/users").then((res) => (users = res));
+        return { users };
     },
     methods: {
-        getMsg() {
-            // helloControllerにアクセス
-            this.$axios
-                .$get("/api/v1/hello")
-                .then((res) => this.msgs.push(res));
+        dateFormat(date) {
+            if (isNaN(new Date(date).getTime())) {
+                // 日付が無効な場合の処理
+                return 'Invalid Date'; // または適切なフォールバック値
+            }
+            const dateTimeFormat = new Intl.DateTimeFormat("ja", {
+                dateStyle: "medium",
+                timeStyle: "short",
+            });
+            return dateTimeFormat.format(new Date(date));
         },
     },
 };
